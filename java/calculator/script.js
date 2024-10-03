5b591c545f0138c1901c1b14644c3f5fde4db8c6
Zeecyber <140038911+Zeecyber@users.noreply.github.com>
2024-10-03 10:29:13 +0500

    Add files via upload

---
+++ java/calculator/script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
+let button_array = Array.from(buttons)
+let string = '';
+button_array.forEach(function(btn){
+    btn.addEventListener('click',function(event){
+        if(event.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            display.value = string;
+        }
+        else if(event.target.innerHTML == 'AC'){
+            string = '';
+            display.value = string;
+        }
+        else if(event.target.innerHTML == '='){
+            string = eval(string);
+            display.value = string;
+        }
+        else{
+            string += event.target.innerHTML;
+            display.value = string;
+        }
+    });
+})
